bed8e4aa6bc12d5308deed93e3823129a6b06559
Maxim Mazurok <maxim@mazurok.com>
2022-07-02 12:58:43 +1000

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 // Client ID and API key from the Developer Console
-const CLIENT_ID = 'xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com';
-const API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const CLIENT_ID =
+  "xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com";
+const API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest'];
+const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest",
+  "https://googleads.googleapis.com/$discovery/rest?version=v11",
+];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/tasks.readonly';
+const SCOPES = [
+  "https://www.googleapis.com/auth/tasks.readonly",
+  "https://www.googleapis.com/auth/adwords",
+];
 
-const authorizeButton = document.getElementById('authorize_button');
-const signoutButton = document.getElementById('signout_button');
+const authorizeButton = document.getElementById("authorize_button");
+const signoutButton = document.getElementById("signout_button");
 
 /**
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+  gapi.load("client:auth2", initClient);
 }
 
 /**
@@ -24,22 +31,28 @@ function handleClientLoad() {
  *  listeners.
  */
 function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES,
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES.join(" "),
+      "developer-token": "XXXXXXXXXXXXXXXXXXXXXX",
+    })
+    .then(
+      function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton && (authorizeButton.onclick = handleAuthClick);
-    signoutButton && (signoutButton.onclick = handleSignoutClick);
-  }, function (error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton && (authorizeButton.onclick = handleAuthClick);
+        signoutButton && (signoutButton.onclick = handleSignoutClick);
+      },
+      function (error) {
+        appendPre(JSON.stringify(error, null, 2));
+      }
+    );
 }
 
 /**
@@ -48,12 +61,12 @@ function initClient() {
  */
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
-    authorizeButton && (authorizeButton.style.display = 'none');
-    signoutButton && (signoutButton.style.display = 'block');
+    authorizeButton && (authorizeButton.style.display = "none");
+    signoutButton && (signoutButton.style.display = "block");
     listTaskLists();
   } else {
-    authorizeButton && (authorizeButton.style.display = 'block');
-    signoutButton && (signoutButton.style.display = 'none');
+    authorizeButton && (authorizeButton.style.display = "block");
+    signoutButton && (signoutButton.style.display = "none");
   }
 }
 
@@ -78,8 +91,8 @@ function handleSignoutClick(event) {
  * @param {string} message Text to be placed in pre element.
  */
 function appendPre(message) {
-  const pre = document.getElementById('content');
-  const textContent = document.createTextNode(message + '\n');
+  const pre = document.getElementById("content");
+  const textContent = document.createTextNode(message + "\n");
   pre && pre.appendChild(textContent);
 }
 
@@ -87,25 +100,52 @@ function appendPre(message) {
  * Print task lists.
  */
 function listTaskLists() {
-  gapi.client.tasks.tasklists.list({
-    maxResults: 10,
-  }).then(function (response) {
-    appendPre('Task Lists:');
-    const taskLists = response.result.items;
-    if (taskLists && taskLists.length > 0) {
-      for (let i = 0; i < taskLists.length; i++) {
-        const taskList = taskLists[i];
-        appendPre(taskList.title + ' (' + taskList.id + ')');
+  // gapi.client.tasks.tasklists
+  //   .list({
+  //     maxResults: 10,
+  //   })
+  //   .then(function (response) {
+  //     appendPre("Task Lists:");
+  //     const taskLists = response.result.items;
+  //     if (taskLists && taskLists.length > 0) {
+  //       for (let i = 0; i < taskLists.length; i++) {
+  //         const taskList = taskLists[i];
+  //         appendPre(taskList.title + " (" + taskList.id + ")");
 
-        taskList.id && gapi.client.tasks.tasks.list({
-          tasklist: taskList.id,
-        }).then((response) => {
-          appendPre('\nTasks:');
-          response.result.items && response.result.items.forEach(task => appendPre(task.title));
-        });
-      }
-    } else {
-      appendPre('No task lists found.');
-    }
-  });
+  //         taskList.id &&
+  //           gapi.client.tasks.tasks
+  //             .list({
+  //               tasklist: taskList.id,
+  //             })
+  //             .then((response) => {
+  //               appendPre("\nTasks:");
+  //               response.result.items &&
+  //                 response.result.items.forEach((task) =>
+  //                   appendPre(task.title)
+  //                 );
+  //             });
+  //       }
+  //     } else {
+  //       appendPre("No task lists found.");
+  //     }
+  //   });
+
+  gapi.client
+    .request({
+      path: "https://content-googleads.googleapis.com/v11/geoTargetConstants:suggest",
+      body: {
+        geoTargets: { geoTargetConstants: ["20118"] },
+        countryCode: "AU",
+      },
+      headers: { "developer-token": "XXXXXXXXXXXXXXXXXXXXXX" },
+    })
+    // gapi.client.googleads.geoTargetConstants
+    //   .suggest(
+    //     {},
+    //     { geoTargets: { geoTargetConstants: ["20118"] }, countryCode: "AU" },
+    //     // @ts-expect-error
+    //     { headers: { "developer-token": "XXXXXXXXXXXXXXXXXXXXXX" } }
+    //   )
+    .then((x) => console.log(x))
+    .catch((x) => console.error(x));
 }
